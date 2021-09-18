1ca12c82398c992194d51ea375d4131c9ef8cb8e
jacksonmccluskey <jacksonmccluskey@protonmail.com>
2021-09-17 21:47:02 -0400

    change port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { json } from 'body-parser';
 const app = express()
 app.use(json)
 
-const PORT = 3000;
+const PORT = 3333;
 
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}...`)
