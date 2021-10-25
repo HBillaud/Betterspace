da2c3e47852463500d6210c8a756d8dd7088c0de
Julia Schneider <juliaschneider@Julias-MacBook-Air.local>
2021-10-25 16:52:33 -0400

    added info to sign up page and added log in page

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import SignUp from './components/SignUp/SignUp'
 import HomePage from './components/HomePage';
+import LogIn from './components/LogIn/LogIn';
 
 function AppRouter() {
   return (
     <Router>
        <div>
+         <Route path="/" exact component={LogIn} />
          <Route path="/signup" exact component={SignUp} />
          <Route path="/home" exact component={HomePage} />
        </div>
--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 
-const SignUp = () => {
+const LogIn = () => {
   const history = useHistory()
   async function handleSubmit(e:any) {
     e.preventDefault()
     const email: string = e.target[0].value;
-    const firstName: string = e.target[1].value;
-    const lastName: string = e.target[2].value;
-    const password: string= e.target[3].value;
-    const isProf: boolean = e.target[4].checked; /* for now just store this */
-    await axios.post(process.env.REACT_APP_SERVER + '/auth/signup', {
+
+    const password: string= e.target[1].value;
+    await axios.post(process.env.REACT_APP_SERVER + '/auth/login', {
       email: email,
       password: password, 
     }) .then(function (response: any) {
@@ -28,22 +26,16 @@ const SignUp = () => {
     <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
 
     <form onSubmit={handleSubmit}>
-      <h3> Create an account </h3>
+      <h3> Log In </h3>
       <label htmlFor="email">Email: </label>
       <input type="text" id="email" name="email"/><br/><br/>
-      <label htmlFor="firstName">First Name: </label>
-      <input type="text" id="firstName" name="firstName"/><br/><br/>
-      <label htmlFor="lastName">Last Name: </label>
-      <input type="text" id="lastName" name="lastName"/><br/><br/>
       <label htmlFor="password">Password: </label>
       <input type="text" id="password" name="password"/><br/><br/>
-      <input type="checkbox" id="teach" name="teach"/>       
-      <label htmlFor="teach">I am a professor.</label><br/><br/>
       <button type="submit">Submit</button>
-      <h5>Already have an account? <a href="/">Log in!</a></h5>
+      <h5>Don't have an account? <a href="/signup">Sign up!</a></h5>
 
     </form>
     </div>
   )  
 }
-export default SignUp;
+export default LogIn;
