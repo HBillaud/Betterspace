f49fbe3ee3e351d9f0a0d5d83b7fe29a1d1759a7
HBillaud <hpcbillaud@gmail.com>
2021-12-14 17:33:31 -0500

    Create form for professor to create assignment for specific course

--- client/src/components/ProfCoursePage.tsx
+++ client/src/components/ProfCoursePage.tsx
@@ -2,12 +2,39 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useStyles from './GridStyles';
 import axios from 'axios';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const ProfCoursePage = (props :any) => {
 
   const params: {id: string, courseName: string} = useParams();
   const [description, setDescription] = useState("");
   const [option, setOption] = useState("");
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+
+  async function handleAssignmentSubmit(e:any) {
+    e.preventDefault()
+    const title: string = e.target[0].value;
+    const description: string = e.target[1].value;
+    const due_date: string = e.target[2].value;
+    const course: string= `${params.courseName}`;
+    await axios.post(process.env.REACT_APP_SERVER + `/v1/professor/${params.id}/courses/${params.courseName}/assignments`, {
+      title: title,
+      description: description,
+      due_date: due_date,
+      course: course,
+    }) .then(function (response: any) {
+      if (response.status === 200) {
+        alert('Assignment successfully created!')
+        window.location.reload();
+      } else {
+        alert('Invalid data! Assignment could not be created.\n' + response.data.message)
+      }
+    }).catch((error) => {
+      console.log(error)
+    })
+
+  }
 
   const courseInfo = () => {
     if(option === "update") {
@@ -18,7 +45,18 @@ const ProfCoursePage = (props :any) => {
       )
     } else if(option === "add") {
       return(
-        <div style={{margin: "25px", display: 'flex', justifyContent: 'center', alignItems: 'center'}}>add</div>
+        <div style={{margin: "25px", display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <form onSubmit={handleAssignmentSubmit}>
+            <h3>Create an assignment</h3>
+            <label htmlFor="title">Title: </label>
+            <input type="text" id="title"/><br/><br/>
+            <label htmlFor="description">Description: </label>
+            <input type="text" id="description"/><br/><br/>
+            <label htmlFor="date">Due Date:</label>
+            <DatePicker selected={startDate} onChange={(date: Date | null) => setStartDate(date)} />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       )
     } else {
       return(
